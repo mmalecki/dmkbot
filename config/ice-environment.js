c3379e8077b491b521a07545349185baaddb0ab4
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-09-02 17:14:39 +0300

    Added some files and a talk spam plugin

---
+++ config/ice-environment.js
@@ -0,0 +1,33 @@
+exports.common = {
+  irc: {
+    nick: "icebot",
+    //channel: ["#oulu", "#oulu-test"]
+    channels: ["#node.js"]
+  },
+  modules: {
+    ping: {},
+    docs: {},
+    up: {},
+    talk: {
+		interval: 15000,
+		file:'./randomstuff.ini'
+	},
+    github: {
+      interval: 15000,
+      repo: 'jheusala/oulu'
+    }
+  }
+}
+
+exports.development = {
+  irc: {
+    host: "irc.kapsi.fi"
+  },
+}
+
+exports.production = {
+  irc: {
+    host: "irc.kapsi.fi"
+  }
+}
+
