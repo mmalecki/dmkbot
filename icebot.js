877e011adfdbaa74822d9d5d876f46c67469e5da
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-09-13 05:50:20 +0300

    Removed temporary file

--- icebot.js
+++
@@ -1,7 +0,0 @@
-var Settings = require('settings');
-var bot = require('./lib/bot.js');
-
-var file = __dirname + '/config/ice-environment.js';
-var settings = new Settings(file).getEnvironment('development');
-
-b = new bot.Bot(settings);
